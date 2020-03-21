9a19ba952d8b8d010678f7465649200fbc9286b8
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-03-21 16:28:49 +0000

    Adding terraform

---
+++ Terraform/function-app.tf
@@ -0,0 +1,38 @@
+resource "azurerm_resource_group" "resource-group" {
+    name = "FM-GeoLocation-${var.environment}"
+    location = "${var.region}"
+}
+
+resource "azurerm_storage_account" "function-app-storage-account" {
+    name                     = "funcappsa${var.environment}"
+    resource_group_name      = "${azurerm_resource_group.resource-group.name}"
+    location                 = "${azurerm_resource_group.resource-group.location}"
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "app-service-plan" {
+    name = "GeoLocation-AppPlan-${var.environment}"
+    resource_group_name = "${azurerm_resource_group.resource-group.name}"
+    location = "${azurerm_resource_group.resource-group.location}"
+    kind = "FunctionApp"
+    sku {
+        tier = "Dynamic"
+        size = "Y1"
+    }
+}
+
+resource "azurerm_function_app" "geolocation-function-app" {
+    name = "geolocation-funcapp-${var.environment}"
+    location = "${azurerm_resource_group.resource-group.location}"
+    resource_group_name = "${azurerm_resource_group.resource-group.name}"
+    storage_connection_string = "${azurerm_storage_account.function-app-storage-account.primary_connection_string}"
+    app_service_plan_id = "${azurerm_app_service_plan.app-service-plan.id}"
+    version = "~3"
+
+    app_settings = {
+        "TableStorageConnectionString" = "${azurerm_storage_account.geolocation-storage.primary_connection_string}"
+        "MaxMindUserId" = "${var.MaxMindUserId}"
+        "MaxMindApiKey" = "${var.MaxMindApiKey}"
+    }
+}
---
+++ Terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "uksouth"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "MaxMindUserId" {
+  default = "1"
+}
+
+variable "MaxMindApiKey" {
+  default = "api-key"
+}
