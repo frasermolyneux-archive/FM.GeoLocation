0a713e3a649fc735d30ad2c84bfbde3fdfe55be9
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-03-26 10:25:04 +0000

    Adding base address to contract

--- src/FM.GeoLocation.Contract/GeoLocationUrls.cs
+++ src/FM.GeoLocation.Contract/GeoLocationUrls.cs
@@ -2,6 +2,11 @@
 {
     public static class GeoLocationUrls
     {
+        public static string LookupAddressBase()
+        {
+            return "https://geo-location.net/Home/LookupAddress/";
+        }
+
         public static string LookupAddress(string address)
         {
             return $"https://geo-location.net/Home/LookupAddress/{address}";
