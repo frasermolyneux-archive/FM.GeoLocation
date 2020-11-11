be301c709f57031c1229cae92b60a058b1b83f80
Fraser Molyneux <websites@mx-mail.io>
2020-11-11 18:43:00 +0000

    Refactoring around terraform

---
+++ terraform/common-components.tf
@@ -0,0 +1,23 @@
+resource "azurerm_resource_group" "resource-group" {
+    name = "FM-GeoLocation-${var.environment}"
+    location = var.region
+}
+
+resource "azurerm_storage_account" "appdata-storage" {
+    name = "geolocationsa${var.environment}"
+    resource_group_name = azurerm_resource_group.resource-group.name
+    location = azurerm_resource_group.resource-group.location
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_table" "geolocation-table" {
+  name = "locations"
+  resource_group_name = azurerm_resource_group.resource-group.name
+  storage_account_name = azurerm_storage_account.geolocation-storage.name
+}
+
+output "appdata_storage_connection" {
+  value = azurerm_storage_account.appdata-storage.primary_connection_string
+  sensitive = true
+}
