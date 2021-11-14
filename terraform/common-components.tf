6e18f1d64d3a1da1a9c3c246c698a27fbb6507f2
Fraser Molyneux <frasermolyneux@mx-mail.io>
2021-11-14 09:29:39 +0000

    Refactor, removing deprecated features

--- terraform/common-components.tf
+++ terraform/common-components.tf
@@ -1,40 +1,32 @@
 resource "azurerm_resource_group" "resource-group" {
-  name = "fm-geolocation-${var.environment}"
+  name     = "fm-geolocation-${var.environment}"
   location = var.region
 }
 
 resource "azurerm_storage_account" "appdata-storage" {
-  name = "geolocationappdata${var.environment}"
-  resource_group_name = azurerm_resource_group.resource-group.name
-  location = azurerm_resource_group.resource-group.location
-  account_tier = "Standard"
+  name                     = "geolocationappdata${var.environment}"
+  resource_group_name      = azurerm_resource_group.resource-group.name
+  location                 = azurerm_resource_group.resource-group.location
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_table" "locations-table" {
-  name = "locations"
+  name                 = "locations"
   storage_account_name = azurerm_storage_account.appdata-storage.name
 }
 
 output "appdata_storage_connection" {
-  value = azurerm_storage_account.appdata-storage.primary_connection_string
+  value     = azurerm_storage_account.appdata-storage.primary_connection_string
   sensitive = true
 }
 
 resource "azurerm_application_insights" "app-insights" {
-  name = "fm-geolocation-appinsights-${var.environment}"
-  location = azurerm_resource_group.resource-group.location
-  resource_group_name = azurerm_resource_group.resource-group.name
-  application_type = "web"
+  name                 = "fm-geolocation-appinsights-${var.environment}"
+  location             = azurerm_resource_group.resource-group.location
+  resource_group_name  = azurerm_resource_group.resource-group.name
+  application_type     = "web"
   daily_data_cap_in_gb = 10
-  retention_in_days = 30
-  disable_ip_masking = true
-}
-
-output "instrumentation_key" {
-  value = azurerm_application_insights.app-insights.instrumentation_key
-}
-
-output "app_id" {
-  value = azurerm_application_insights.app-insights.app_id
+  retention_in_days    = 30
+  disable_ip_masking   = true
 }
--- terraform/func-app.tf
+++ terraform/func-app.tf
@@ -1,32 +1,33 @@
 resource "azurerm_storage_account" "funcapp-storage-account" {
-    name = "geolocationfuncapp${var.environment}"
-    resource_group_name = azurerm_resource_group.resource-group.name
-    location = azurerm_resource_group.resource-group.location
-    account_tier = "Standard"
-    account_replication_type = "LRS"
+  name                     = "geolocationfuncapp${var.environment}"
+  resource_group_name      = azurerm_resource_group.resource-group.name
+  location                 = azurerm_resource_group.resource-group.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
 resource "azurerm_app_service_plan" "funcapp-service-plan" {
-    name = "geolocation-appsvcplan-${var.environment}"
-    resource_group_name = azurerm_resource_group.resource-group.name
-    location = azurerm_resource_group.resource-group.location
-    kind = "FunctionApp"
-    sku {
-        tier = "Dynamic"
-        size = "Y1"
-    }
+  name                = "geolocation-appsvcplan-${var.environment}"
+  resource_group_name = azurerm_resource_group.resource-group.name
+  location            = azurerm_resource_group.resource-group.location
+  kind                = "FunctionApp"
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
 }
 
 resource "azurerm_function_app" "function-app" {
-    name = "geolocation-funcapp-${var.environment}"
-    location = azurerm_resource_group.resource-group.location
-    resource_group_name = azurerm_resource_group.resource-group.name
-    storage_connection_string = azurerm_storage_account.funcapp-storage-account.primary_connection_string
-    app_service_plan_id = azurerm_app_service_plan.funcapp-service-plan.id
+  name                       = "geolocation-funcapp-${var.environment}"
+  location                   = azurerm_resource_group.resource-group.location
+  resource_group_name        = azurerm_resource_group.resource-group.name
+  storage_account_name       = azurerm_storage_account.funcapp-storage-account.name
+  storage_account_access_key = azurerm_storage_account.funcapp-storage-account.primary_access_key
+  app_service_plan_id        = azurerm_app_service_plan.funcapp-service-plan.id
 
-    version = "~4"
+  version = "~4"
 
-    site_config {
-      dotnet_framework_version = "v6.0"
-    }
+  site_config {
+    dotnet_framework_version = "v6.0"
+  }
 }
